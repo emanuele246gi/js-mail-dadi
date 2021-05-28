05950547884c5a59292baa7c58ebdb48d62ce566
Emanuele <emanuele65b@gmail.com>
2021-05-28 16:22:15 +0200

    add ids e if

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// Primo dado
 var primo = Math.floor(Math.random() * 6 + 1);
 
+
+// Secondo dado
 var secondo = Math.floor(Math.random() * 6 + 1);
+
+// Risultato
+var risultato;
+
+if (primo > secondo){
+    risultato = Vittoria!
+}
